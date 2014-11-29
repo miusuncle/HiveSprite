2495b71669fbbb3bd27063c881a34a8a85615b87
miusuncle <miusuncle@gmail.com>
2014-11-29 13:30:40 +0800

    Augument `on`'s functionality

--- HiveSpriteOnly/lib/on.jsx
+++ HiveSpriteOnly/lib/on.jsx
@@ -2,33 +2,48 @@ var _ = require('./underscore');
 
 var on = function (target, type, listener) {
   if (_.isObject(type)) {
-    return on.multiple(target, type);
+    return on.associate(target, type);
   }
 
-  if (typeof target.on === 'function' && typeof type !== 'function') {
+  if (typeof target.on === 'function') {
     target.on(type, listener);
     return {
       remove: _(target.off).bind(target, type, listener)
     };
   }
 
+  var eventSplitter = /\s+/;
+  var parts = _.compact(type.split(eventSplitter));
+
+  if (parts.length >= 2) {
+    return on.multipart(target, parts, listener);
+  }
+
+  type = parts.pop();
   var uber = type.match(/(.+):(.+)/);
 
   if (uber) {
     return on.delegate.apply(on, uber.slice(-2))(target, listener);
-  } else {
-    target.addEventListener(type, listener, false);
-    return {
-      remove: function () {
-        target.removeEventListener(type, listener, false);
-      }
-    };
   }
+
+  return on.plain(target, type, listener);
 };
 
 module.exports = _.extend(on, {
-  multiple: function (target, pairs) {
+  associate: function (target, pairs) {
     var handlers = _.reduce(pairs, function (ret, listener, type) {
+      return _(ret).push(on(target, type, listener));
+    }, []);
+
+    handlers.remove = function () {
+      _.each(handlers, _.partial(_.result, _, 'remove'));
+    };
+
+    return handlers;
+  },
+
+  multipart: function (target, types, listener) {
+    var handlers = _.reduce(types, function (ret, type) {
       return _(ret).push(on(target, type, listener));
     }, []);
 
@@ -47,6 +62,15 @@ module.exports = _.extend(on, {
           listener.call(eventTarget, event);
         }
       });
+    };
+  },
+
+  plain: function (target, type, listener) {
+    target.addEventListener(type, listener, false);
+    return {
+      remove: function () {
+        target.removeEventListener(type, listener, false);
+      }
     };
   }
 });
